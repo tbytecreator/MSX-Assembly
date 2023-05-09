19a0b98f8276de07f6be9c140dcf3ca25138de53
Manoel Neto <113383491+tbytecreator@users.noreply.github.com>
2023-05-09 19:26:58 -0300

    Acertos de compilação

--- CRIPTO/MSX/Assets/Variaveis.asm
+++ CRIPTO/MSX/Assets/Variaveis.asm
@@ -4,13 +4,10 @@
 ; Manoel Neto 2019-09-25
 ; Variaveis utilizadas no Criptologic Z80
 ; =============================================================================
-romSize             equ 8192        ; O tamanho que a ROM deve ter
-romArea             equ &4000       ; Minha ROM começa aqui
-ramArea             equ &E000       ; Inicio da área de variáveis
 NumTamFrase 	    equ ramArea	    ; Variavel => Tamanho da entrada Jog 1
-NumAleatorio 		equ ramArea+1	  ; Variavel => Numero sorteado
-NumSorteios 		equ ramArea+2	  ; Variavel => Numero de sorteios realizados
-NumDivIdeal 		equ ramArea+3	  ; Variavel => Divisor ideal
+NumAleatorio 		equ ramArea+1   ; Variavel => Numero sorteado
+NumSorteios 		equ ramArea+2	; Variavel => Numero de sorteios realizados
+NumDivIdeal 		equ ramArea+3	; Variavel => Divisor ideal
 MatSorteados        equ ramArea+4   ; Matriz => Numeros sorteados
 StrFrase            equ ramArea+19  ; String => frase digitada
 NumPosSort		    equ ramArea+35  ; Variavel => PosicaoSorteada
