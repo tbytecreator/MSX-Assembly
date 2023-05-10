58ac31d6fbb62d52863900830c1c697a54a41126
Manoel Neto <113383491+tbytecreator@users.noreply.github.com>
2023-05-09 22:36:22 -0300

    Sorteio de 1 a 14

--- CRIPTO/MSX/Embaralhar.asm
+++ CRIPTO/MSX/Embaralhar.asm
@@ -6,18 +6,18 @@
 ; Imprimir a Frase Embaralhada
 ;==============================================================================
 Embaralhar:
-	ld a,0									; prepara primeira passada
+	ld a,0							; prepara primeira passada
 	ld (NumContEmb),a				; zera o contador de embaralhamento
 GravarProxima:
 	call AcharPosSort				; achar a posicao sorteada
 	call AcharLetra					; acha a letra dessa passada
 	call GravarLetra				; gravar a letra dessa passada
-	ld a,(NumTamFrase)			; pega para comparar
-	ld b,a									; o tamnho da frase
+	ld a,(NumTamFrase)			    ; pega para comparar
+	ld b,a							; o tamnho da frase
 	ld a,(NumContEmb)				; com o contador de embaralhamento
-	cp b										; se pegamos todos
+	cp b							; se pegamos todos
 	jp z,GravouTudo					; gravamos tudo
-	inc a 									; senao vamos para a proxima
+	inc a 							; senao vamos para a proxima
 	ld (NumContEmb),a				; e guardamos no contador
 	jp GravarProxima				; pega a proxima
 GravouTudo:
@@ -40,7 +40,7 @@ ret
 
 AcharLetra:
 	ld a,(NumPosSort)
-	dec a 												; enderecos comecam com 0
+	dec a 							; enderecos comecam com 0
 	ld hl,StrFrase
 LoopAcharLetra:
 	cp 0
