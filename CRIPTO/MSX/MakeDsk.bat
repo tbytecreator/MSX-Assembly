eb3a9fe1ee86e67d6e0de6b257d262e8e561f88f
Manoel Neto <113383491+tbytecreator@users.noreply.github.com>
2023-10-31 14:49:48 -0300

    Disco para patreon

--- CRIPTO/MSX/MakeDsk.bat
+++ CRIPTO/MSX/MakeDsk.bat
@@ -4,3 +4,4 @@ del DSK\CRIPTDSK.BIN
 echo ======= Compilando nova BIN ======
 pasmo --msx CRIPTDSK.ASM DSK\CRIPTDSK.BIN
 echo ============= Pronto =============
+openmsx -machine Gradiente_Expert_GPC-1 -ext DDX_3.0 -diska DSK\.
