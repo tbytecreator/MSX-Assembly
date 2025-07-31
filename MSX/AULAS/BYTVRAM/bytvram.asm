11cc96364e3d29a02c732dad5d73c1cfcb63abb8
Manoel Neto <113383491+tbytecreator@users.noreply.github.com>
2025-07-31 20:40:02 -0300

    copia de byte na VRAM

--- MSX/AULAS/BYTVRAM/bytvram.asm
+++ MSX/AULAS/BYTVRAM/bytvram.asm
@@ -2,10 +2,13 @@
 ; Copiando bytes para VRAM
 ; =====================================
 include "BIOS.ASM"
-
 org 0xC000
+	db 0XFE	
+ 	dw STARTCODE
+ 	dW ENDCODE
+ 	dW STARTCODE
 STARTCODE:
-	ld hl,0x6912		; Endereco de memoria VRAM
+	ld hl,0x1B01		; Endereco de memoria VRAM
 	ld a,(0XC00A)		; Pego o valor do endereco
 	call WRTVRM
 ; =====================================
