344a61c8f7cb02d33d165056e2600fc896931c04
tbytecreator <tbytecreator@gmail.com>
2025-08-01 08:31:35 -0300

    Acertos para a aula

--- MSX/AULAS/BYTVRAM/bytvram.asm
+++ MSX/AULAS/BYTVRAM/bytvram.asm
@@ -9,10 +9,10 @@ org 0xD000
  	dW ENDCODE
  	dW STARTCODE
 STARTCODE:
-	ld hl,0x1B01		; Endereco de memoria VRAM
-	ld a,(0xC00A)		; Pego o valor do endereco
-	call WRTVRM			;
-ret
+	ld hl,0x1B01	; Endereco de memoria VRAM
+	ld a,(0xC00A)	; Pego o valor do endereco
+	call WRTVRM		; gravo o byte na VRAM
+ret					; retorna ao BASIC
 ; =====================================
 ; FIM PROGRAMA
 ; =====================================
