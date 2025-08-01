79d9b7ea1d771948e743483bff6588be724a7a9e
tbytecreator <tbytecreator@gmail.com>
2025-08-01 08:26:10 -0300

    Faltou o RET

--- MSX/AULAS/BYTVRAM/bytvram.asm
+++ MSX/AULAS/BYTVRAM/bytvram.asm
@@ -2,15 +2,17 @@
 ; Copiando bytes para VRAM
 ; =====================================
 include "BIOS.ASM"
-org 0xC000
-	db 0XFE	
+
+org 0xD000
+	db 0xFE	
  	dw STARTCODE
  	dW ENDCODE
  	dW STARTCODE
 STARTCODE:
 	ld hl,0x1B01		; Endereco de memoria VRAM
-	ld a,(0XC00A)		; Pego o valor do endereco
-	call WRTVRM
+	ld a,(0xC00A)		; Pego o valor do endereco
+	call WRTVRM			;
+ret
 ; =====================================
 ; FIM PROGRAMA
 ; =====================================
