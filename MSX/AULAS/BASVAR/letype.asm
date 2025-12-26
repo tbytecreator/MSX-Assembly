3a6b4a7cbc14e9ced8c79ca611cca1e380cb7447
Manoel Neto <113383491+tbytecreator@users.noreply.github.com>
2025-12-25 21:36:04 -0300

    ler tipo passado

--- MSX/AULAS/BASVAR/letype.asm
+++ MSX/AULAS/BASVAR/letype.asm
@@ -5,23 +5,12 @@
 ; 4 = Single (!)
 ; 8 = Double (#)
 
-org     0E000h          ; Endereço de início típico para código assembly MSX
+org 0E000h          ; Endereço de início típico para código assembly MSX
 
-VALTYP: equ     0F663h      ; Endereço do VALTYP no MSX BASIC
+VALTYP: equ 0F663h  ; Endereço do VALTYP no MSX BASIC
 
-inicio:
-    push    hl              ; Preserva registradores
-    push    de
-    push    bc
-    
-    ld      a,(VALTYP)      ; Lê o tipo da variável do BASIC
-    ld      l,a             ; Coloca o tipo em L
-    ld      h,0             ; H = 0 para retornar valor em HL
-    
-    pop     bc              ; Restaura registradores
-    pop     de
-    pop     hl
-    
-    ret                     ; Retorna para o BASIC
-
-    end
+inicio:  
+    ld a,(VALTYP)   ; Lê o tipo da variável do BASIC
+    ld (0F7F8h),a   ; Armazena temporariamente em memória
+    ld a,2          ; Coloca o tipo em L   
+    ret             ; Retorna para o BASIC
